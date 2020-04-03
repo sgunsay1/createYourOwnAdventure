6285e82ca590c59f92bd08dc584e18f59d0e8088
Seth Gunsay <sgunsay1@gmail.com>
2020-04-03 11:27:40 -0600

    Made buttons stack

--- src/components/btnContainer.js
+++ src/components/btnContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { Button } from '@material-ui/core'
-import { red, green, blue, orange } from "@material-ui/core/colors";
+import { red, green, blue, orange, purple } from "@material-ui/core/colors";
 
 function BtnContainer({
   classes,
@@ -22,11 +22,12 @@ function BtnContainer({
 }
 
 function RenderBtn({ btnTxt, path, clickHandler, colorID }) {
-  switch (colorID % 4) {
-    case 0: return <RedBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</RedBtn>
-    case 1: return <GreenBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</GreenBtn>
-    case 2: return <BlueBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</BlueBtn>
-    case 3: return <OrangeBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</OrangeBtn>
+  switch (colorID % 5) {
+    case 0: return <div><RedBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</RedBtn></div>
+    case 1: return <div><GreenBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</GreenBtn></div>
+    case 2: return <div><BlueBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</BlueBtn></div>
+    case 3: return <div><OrangeBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</OrangeBtn></div>
+    case 4: return <div><PurpleBtn variant="contained" color="primary" onClick={() => clickHandler(path)}>{btnTxt}</PurpleBtn></div>
     default: return
   }
 
@@ -70,6 +71,16 @@ const OrangeBtn = withStyles(theme => ({
     backgroundColor: orange[500],
     "&:hover": {
       backgroundColor: orange[700]
+    }
+  }
+}))(Button);
+
+const PurpleBtn = withStyles(theme => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    "&:hover": {
+      backgroundColor: purple[700]
     }
   }
 }))(Button);
